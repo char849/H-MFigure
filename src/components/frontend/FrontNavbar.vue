82e83d8525fa0330a5da7cd585d9340420011860
charlotte <charlottelee849@gmail.com>
2024-03-09 01:27:49 +0800

    3/9

--- src/components/frontend/FrontNavbar.vue
+++ src/components/frontend/FrontNavbar.vue
@@ -1,15 +1,15 @@
 <template>
-  <nav class="navbar navbar-expand-lg sticky-top bg-primary">
+  <nav class="navbar navbar-expand-lg sticky-top bg-primary position-relative">
     <div class="container">
       <button
-        class="navbar-toggler"
+        class="navbar-toggler position-absolute"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarNav"
       >
         <img src="/img/bars.svg" alt="bars" />
       </button>
-      <RouterLink class="navbar-brand ms mx-md-auto d-lg-none" to="/">
+      <RouterLink class="navbar-brand mx-auto d-lg-none" to="/">
         <img
           src="https://storage.googleapis.com/vue-course-api.appspot.com/charlotte-lee849/1707749509526.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=GXFEqpQy04CExUb7x4STbngGE0KIedpcFYbd6aw6OWElk1a13FI%2FZBV7QUdPhIwwiN3OYzyl4XQfPnBMPIVO76gvrzZLptxpXGE%2BT7KECZ435Q0uo%2BaQ0jBD0tYAd%2F0MFXCFKlT9KAWhsukV7%2F8yFXVI%2FTIAsxXsts3MB29%2FvnKv36x%2Bjb0fz7Z2mQofOXHoTEqKscvgfknyK4OoxxuErk0wswWiXu1eVeEmyAB%2BK6Nqhnj%2Fu1f0icBEA4CstMJGqMbNSsCRcUCpPD%2BmRPHN9Z1I403Q6UneyVOPjuFTVMkEmcydyiVyP6taAMqTRsPaFosUMVuy1RgVJOGyWH1GSQ%3D%3D"
           alt="logo"
@@ -32,7 +32,7 @@
        >
         <ul
           class="navbar-nav ms-lg-0 ms-lg-0 ms-xl-7 ms-xxl-9 d-flex align-items-center
-           fw-bold rounded-5"
+           fw-bold"
         >
           <li class="nav-item active mx-auto me-lg-5 me-xl-5">
             <RouterLink class="nav-link" to="/products"

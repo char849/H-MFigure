862f29c7f305313c5f37a521cb2b9e79bd513d55
charlotte <charlottelee849@gmail.com>
2024-03-09 20:41:49 +0800

    3/9

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -13,18 +13,14 @@ const useCounterStore = defineStore('cartStore', () => {
   const carts = ref([]);
   const finalTotal = ref(0);
   const total = ref(0);
-  const cartsLength = ref(0);
   const isLoading = ref(false);
 
   const getCart = () => {
     axios.get(`${VITE_API}api/${VITE_PATH}/cart`)
       .then((res) => {
-        // cart.value.splice(0);
-        // cart.value.push(...res.data.data.carts);
         carts.value = res.data.data.carts;
         finalTotal.value = res.data.data.final_total;
         total.value = res.data.data.total;
-        cartsLength.value = carts.value.length;
         console.log('pinia cart', carts.value);
       })
       .catch((error) => {

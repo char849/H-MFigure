134b5643eb1e701c093571f9a45a48f05a258e50
charlotte <charlottelee849@gmail.com>
2024-03-11 00:11:12 +0800

    3/11

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,8 +4,8 @@
     <p class="display-3 text-info my-4 mb-6 mx-2">
         Not Found 404!!
     </p>
-    <RouterLink to="/products" class="fs-5 btn btn-danger rounded-5 py-3 px-5">
-      課程介紹 !
+    <RouterLink to="/" class="fs-5 btn btn-danger rounded-5 py-3 px-5">
+      返回模型課程回首頁 !
     </RouterLink>
   </div>
 </template>
